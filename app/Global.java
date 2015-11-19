7fb2ae15fe23c2d0f2cc2112b10d6c678c4da5e9
Antonio Lúcio <antoniolucio_@hotmail.com>
2015-11-19 12:42:12 -0200

    Mudanças na classe global
    Adicionamos novas disciplinas
    Erro comentado em adicionar usuários

--- app/Global.java
+++ app/Global.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import models.Disciplina;
 import models.Tema;
+import models.User;
 import models.dao.GenericDAOImpl;
 import play.Application;
 import play.GlobalSettings;
@@ -14,6 +15,7 @@ public class Global extends GlobalSettings {
 
 	private static GenericDAOImpl dao = new GenericDAOImpl();
 	private List<Disciplina> disciplinas = new ArrayList<>();
+//	private List<User> usuarios = new ArrayList<>();
 	
 	@Override
 	public void onStart(Application app) {
@@ -25,6 +27,11 @@ public class Global extends GlobalSettings {
 				if(dao.findAllByClassName(Disciplina.class.getName()).size() == 0){
 					criaDisciplinaTemas();
 				}
+//				
+//				if(dao.findAllByClassName(User.class.getName()).size() == 0){
+//					criaUsuarios();
+//				}
+				
 			}
 		});
 	}
@@ -38,9 +45,28 @@ public class Global extends GlobalSettings {
 	        disciplinas = dao.findAllByClassName("Disciplina");
 
 	        for (Disciplina disciplina: disciplinas) {
-	        dao.removeById(Disciplina.class, disciplina.getId());
+	        	dao.removeById(Disciplina.class, disciplina.getId());
 	       } 
+	        
+//	        usuarios = dao.findAllByClassName("User");
+//	        
+//	        for (User user : usuarios) {
+//				dao.removeById(User.class, user.getId());
+//			}
+//	        
 	    }}); 
+	}
+	
+	private void criaUsuarios(){
+	
+//		for (int i = 0; i < 10; i++) {
+		
+			User usuario = new User("usuario@hotmail.com", "whiskyouaguadecoco", "tantofaz");
+			dao.persist(usuario);
+//		}
+		
+		dao.flush();
+		
 	}
 	
 	private void criaDisciplinaTemas(){
@@ -57,7 +83,28 @@ public class Global extends GlobalSettings {
 		si1.addTema(new Tema("Labs"));
 		si1.addTema(new Tema("Minitestes"));
 		si1.addTema(new Tema("Projeto"));
+		
+		Disciplina calculo = new Disciplina("Cálculo 2");
+		calculo.addTema(new Tema("Integral imprópria"));
+		calculo.addTema(new Tema("Aplicações de integrais"));
+		calculo.addTema(new Tema("Sequências"));
+		calculo.addTema(new Tema("Séries numéricas"));
+		calculo.addTema(new Tema("Intervalor e raio de convergência"));
+		calculo.addTema(new Tema("Séries de potências"));
+		calculo.addTema(new Tema("Funções de vetoriais"));
+		
+		
+		Disciplina eda = new Disciplina("Estrutura de Dados e Algoritmos");
+		eda.addTema(new Tema("Custo de algoritmos"));
+		eda.addTema(new Tema("Algoritmos de ordenação"));
+		eda.addTema(new Tema("Estrutura de dados"));
+		
+		
 		dao.persist(si1);
+		dao.persist(calculo);
+		dao.persist(eda);
 		dao.flush();
 	}
+	
+	
 }
