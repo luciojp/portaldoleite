872bd27aea0a489e51e3870d05de2f397204566a
wendleypf <wendley.franca@ccc.ufcg.edu.br>
2015-11-19 15:40:14 -0300

    Modificação classe Global - criação de usuários funcionando

--- app/Global.java
+++ app/Global.java
@@ -10,13 +10,11 @@ import play.GlobalSettings;
 import play.Logger;
 import play.db.jpa.JPA;
 
-
 public class Global extends GlobalSettings {
 
 	private static GenericDAOImpl dao = new GenericDAOImpl();
 	private List<Disciplina> disciplinas = new ArrayList<>();
-//	private List<User> usuarios = new ArrayList<>();
-	
+
 	@Override
 	public void onStart(Application app) {
 		Logger.info("Aplicação inicializada...");
@@ -24,52 +22,44 @@ public class Global extends GlobalSettings {
 		JPA.withTransaction(new play.libs.F.Callback0() {
 			@Override
 			public void invoke() throws Throwable {
-				if(dao.findAllByClassName(Disciplina.class.getName()).size() == 0){
+				if (dao.findAllByClassName(Disciplina.class.getName()).isEmpty()) {
 					criaDisciplinaTemas();
 				}
-//				
-//				if(dao.findAllByClassName(User.class.getName()).size() == 0){
-//					criaUsuarios();
-//				}
-				
+
+				if (dao.findAllByClassName(User.class.getName()).isEmpty()) {
+					criaUsuarios();
+				}
 			}
 		});
 	}
-	
-	@Override
-	public void onStop(Application app){
-	    JPA.withTransaction(new play.libs.F.Callback0() {
-	    @Override
-	    public void invoke() throws Throwable {
-	        Logger.info("Aplicação finalizando...");
-	        disciplinas = dao.findAllByClassName("Disciplina");
 
-	        for (Disciplina disciplina: disciplinas) {
-	        	dao.removeById(Disciplina.class, disciplina.getId());
-	       } 
-	        
-//	        usuarios = dao.findAllByClassName("User");
-//	        
-//	        for (User user : usuarios) {
-//				dao.removeById(User.class, user.getId());
-//			}
-//	        
-	    }}); 
+	@Override
+	public void onStop(Application app) {
+		JPA.withTransaction(new play.libs.F.Callback0() {
+			@Override
+			public void invoke() throws Throwable {
+				Logger.info("Aplicação finalizando...");
+
+				disciplinas = dao.findAllByClassName("Disciplina");
+
+				for (Disciplina disciplina : disciplinas) {
+					dao.removeById(Disciplina.class, disciplina.getId());
+				}
+			}
+		});
 	}
-	
-	private void criaUsuarios(){
-	
-//		for (int i = 0; i < 10; i++) {
-		
-			User usuario = new User("usuario@hotmail.com", "whiskyouaguadecoco", "tantofaz");
-			dao.persist(usuario);
-//		}
-		
-		dao.flush();
-		
+
+	private void criaUsuarios() {
+		for (int i = 0; i < 10; i++) {
+			User user = new User("DiogoMeloBarbosa" + i + "@jourrapide.com", "1234", "operep" + i);
+			user.setNome("Diogo " + i + "° Melo Barbosa");
+
+			dao.persist(user);
+			dao.flush();
+		}
 	}
-	
-	private void criaDisciplinaTemas(){
+
+	private void criaDisciplinaTemas() {
 		Disciplina si1 = new Disciplina("Sistemas de Informação 1");
 		si1.addTema(new Tema("Análise x Design"));
 		si1.addTema(new Tema("Orientação a objetos"));
@@ -83,7 +73,7 @@ public class Global extends GlobalSettings {
 		si1.addTema(new Tema("Labs"));
 		si1.addTema(new Tema("Minitestes"));
 		si1.addTema(new Tema("Projeto"));
-		
+
 		Disciplina calculo = new Disciplina("Cálculo 2");
 		calculo.addTema(new Tema("Integral imprópria"));
 		calculo.addTema(new Tema("Aplicações de integrais"));
@@ -92,19 +82,16 @@ public class Global extends GlobalSettings {
 		calculo.addTema(new Tema("Intervalor e raio de convergência"));
 		calculo.addTema(new Tema("Séries de potências"));
 		calculo.addTema(new Tema("Funções de vetoriais"));
-		
-		
+
 		Disciplina eda = new Disciplina("Estrutura de Dados e Algoritmos");
 		eda.addTema(new Tema("Custo de algoritmos"));
 		eda.addTema(new Tema("Algoritmos de ordenação"));
 		eda.addTema(new Tema("Estrutura de dados"));
-		
-		
+
 		dao.persist(si1);
 		dao.persist(calculo);
 		dao.persist(eda);
 		dao.flush();
 	}
-	
-	
+
 }
